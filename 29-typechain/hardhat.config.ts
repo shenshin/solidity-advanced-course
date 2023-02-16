9707fb04e14c8721248a1c62a4d33bab23e1cd50
Aleksandr Shenshin <shenshin@me.com>
2023-02-16 21:56:50 +0100

    29-typechain

---
+++ 29-typechain/hardhat.config.ts
@@ -0,0 +1,32 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+const mnemonic: string = require('../.secret.json').mnemonic;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rsktestnet: {
+      chainId: 31,
+      url: 'https://public-node.testnet.rsk.co/',
+      accounts: {
+        mnemonic,
+        path: "m/44'/60'/0'/0",
+      },
+    },
+  },
+};
+
+export default config;
